b074d3a634a2ba5e8e4864b9bd47c3988a7f94a0
cyl03 <cyl03@DESKTOP-J5OPL7O>
2021-08-02 05:57:11 +0900

    ddddddddddddddddddddddd

--- MARIO_BREAKER/src/mario_break/Block.java
+++ MARIO_BREAKER/src/mario_break/Block.java
@@ -6,12 +6,16 @@ public class Block extends RectangleShape{
 	private boolean broke;                            //블럭이 깨지는걸 boolean으로 표현
 	
 	public Block(int x, int y) {
+		
 		setBlock(x,y);
 	}
 	
 	private void setBlock(int x, int y) {             //처음 위치 시킬때 broke 값 false라 화면에 나타나게 된다
 		this.x = x;
 		this.y = y;
+		int d;
+		
+		
 		broke=false;   
 		
 		shape();
